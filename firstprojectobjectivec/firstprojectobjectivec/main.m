fe254ab6b5e67ffe5abe792576ff2a3ada299c67
pujansheth <pujan.rock@yahoo.com>
2014-05-06 13:53:35 -0400

    first project
    hello world example intro to object

---
+++ firstprojectobjectivec/firstprojectobjectivec/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  firstprojectobjectivec
+//
+//  Created by User1 on 2014-05-06.
+//  Copyright (c) 2014 pujansheth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
